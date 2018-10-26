04e7e8351390fcea0ba0c5947e20c8b9cbfbeccc
Quentin Read <qread@rstudio03.research.sesync.org>
2018-10-26 12:05:15 -0400

    View centers of old land use tiles

--- NLCD/old_landuse.r
+++ NLCD/old_landuse.r
@@ -12,10 +12,39 @@ rasters1 <- map(imgfiles, raster)
 
 spplot(rasters1[[1]]) 
 
-tile <- readOGR('/nfs/qread-data/USGS_LC/tilepoly', dsn = 'tilepoly')
+extents1 <- map(rasters1, extent)
+centers1 <- map_dfr(extents1, ~ data.frame(x = (.@xmin + .@xmax)/2, y = (.@ymin + .@ymax)/2))
+
+# Make a map to see where it is
+
+states <- read.csv('/nfs/qread-data/states_albers.csv')
+
+library(ggplot2)
+
+ggplot() +
+  geom_path(data = states, aes(x = long, y = lat, group = group)) +
+  geom_point(data = unique(centers1), aes(x = x, y = y), color = 'red')
+
+
+# go through and get all the locations.
+dir_names <- paste0('Eco', c(paste0('0', 1:9), as.character(10:84)))
+
+all_centers <- map_dfr(dir_names, function(d) {
+  imgfiles <- list.files(file.path('/nfs/qread-data/USGS_LC', d), pattern = '*.img', recursive = TRUE, full.names = TRUE)
+  rasters <- map(imgfiles, function(i) try(raster(i), TRUE))
+  rasters <- rasters[!map_lgl(rasters, inherits, 'try-error')]
+  extents <- map(rasters, extent)
+  map_dfr(extents, ~ data.frame(region = d, x = (.@xmin + .@xmax)/2, y = (.@ymin + .@ymax)/2))
+})
+
+ggplot() +
+  geom_path(data = states, aes(x = long, y = lat, group = group)) +
+  geom_point(data = unique(all_centers), aes(x = x, y = y, color = region)) +
+  theme(legend.position = 'none')
 
 ##################
 # raster format.
 
 giras1 <- raster('/nfs/qread-data/USGS_LC/giras1.tif')
 giras <- raster('/nfs/qread-data/USGS_LC/giras.vrt')
+tile <- readOGR(dsn = '/nfs/qread-data/USGS_LC/tilepoly', layer = 'tilepoly')
