dac6bc877df6debfa6ee0cd6591e6dd90aed61f2
Quentin Read <quentin.read@gmail.com>
2018-12-03 17:17:33 -0500

    Make plots of FLW by LAFA type for all food groups

--- LAFA/viz_lafa.r
+++ LAFA/viz_lafa.r
@@ -2,6 +2,13 @@
 
 source('read_data/read_lafa.r')
 
+# Function to get the most recent year of data for each category.
+most_recent <- function(dat) {
+  dat %>% 
+    group_by(Category) %>%
+    filter(Year == if_else(any(!is.na(Loss_from_primary_to_retail_weight_Percent)), max(Year[!is.na(Loss_from_primary_to_retail_weight_Percent)]), max(Year))) %>%
+    ungroup
+}
 
 # Manipulate and plot fruit data ------------------------------------------
 
@@ -48,8 +55,7 @@ group_names <- c('citrus', 'noncitrus', 'all', 'berries')
 fruit <- fruit %>%
   mutate(is_group = Food_type %in% group_names)
 
-fruit2015 <- fruit %>%
-  filter(Year == 2015)
+fruit2015 <- most_recent(fruit)
 
 # Do calculations to get the proportions.
 fruit2015 <- fruit2015 %>%
@@ -113,7 +119,7 @@ fruit_colplot <- fruit_plotdata %>%
   scale_y_continuous(labels = scales::percent, limits = c(0, 1.02), expand = c(0,0)) +
   th_bar +
   scale_fill_manual(name = 'Proportion', values = cols_bar, labels = c('Processor loss', 'Retailer loss', 'Consumer loss\n(avoidable)', 'Nonedible', 'Eaten')) +
-  ggtitle('Fruit FLW by weight (LAFA)', 'Black box is proportion available to consumer')
+  ggtitle('Fruit FLW by weight (LAFA)', 'Black box comprises the eaten portion plus "unavoidable" waste')
 
 ggsave('~/Dropbox/projects/foodwaste/Results/lafa_fruit_flw.pdf', fruit_colplot, height = 5, width = 6)
 
@@ -152,8 +158,7 @@ veg_group_names <- c('all', 'legumes', 'other beans', 'edible beans')
 veg <- veg %>%
   mutate(is_group = Food_type %in% veg_group_names)
 
-veg2015 <- veg %>%
-  filter(Year == 2015)
+veg2015 <- most_recent(veg)
 
 # Calculations for heights of stacking bars
 veg_plotdata <- veg2015 %>%
@@ -163,3 +168,254 @@ veg_plotdata <- veg2015 %>%
          nonedible = (1 - processor_loss) * (1 - retailer_loss) * Loss_at_consumer_level_Nonedible_share_Percent/100,
          eaten = 1 - (processor_loss + retailer_loss + consumer_loss + nonedible)) %>%
   select(Category, Prep_type, Food_type, is_group, processor_loss, retailer_loss, consumer_loss, nonedible, eaten)
+
+# Make data to draw rectangle around the available portion.
+veg_avail <- veg_plotdata %>%
+  filter(!Prep_type %in% 'all', !is_group) %>%
+  filter(complete.cases(.)) %>%
+  mutate(available = nonedible + eaten)
+
+veg_plotdata %>%
+  filter(!Prep_type %in% 'all', !is_group) %>%
+  filter(complete.cases(.)) %>%
+  mutate(Prep_type = factor(Prep_type, levels = c('fresh', 'canned', 'frozen', 'dried', 'chips'))) %>%
+  melt(id.vars = 1:4, value.name = 'proportion') %>%
+  ggplot(aes(x = Prep_type, y = proportion, fill = variable)) +
+  facet_wrap(~ Food_type) +
+  geom_col() +
+  geom_col(data = veg_avail, aes(y = available), fill = 'transparent', color = 'black', size = 1.5) +
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1.02), expand = c(0,0)) +
+  th_bar +
+  scale_fill_manual(name = 'Proportion', values = cols_bar, labels = c('Processor loss', 'Retailer loss', 'Consumer loss\n(avoidable)', 'Nonedible', 'Eaten')) +
+  ggtitle('Vegetable FLW by weight (LAFA)', 'Black box is proportion available to consumer')
+
+# Plot only a small subset
+the_veggies <- c('sweet corn', 'potatoes', 'broccoli', 'snap beans', 'cabbage', 'spinach')
+
+veg_colplot <- veg_plotdata %>%
+  filter(!Prep_type %in% 'all', !is_group, Food_type %in% the_veggies) %>%
+  filter(complete.cases(.)) %>%
+  mutate(Prep_type = factor(Prep_type, levels = c('fresh', 'canned', 'frozen', 'dried', 'chips'))) %>%
+  melt(id.vars = 1:4, value.name = 'proportion') %>%
+  ggplot(aes(x = Prep_type, y = proportion, fill = variable)) +
+  facet_wrap(~ Food_type) +
+  geom_col() +
+  geom_col(data = veg_avail %>% filter(Food_type %in% the_veggies), aes(y = available), fill = 'transparent', color = 'black', size = 1.5) +
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1.02), expand = c(0,0)) +
+  th_bar +
+  scale_fill_manual(name = 'Proportion', values = cols_bar, labels = c('Processor loss', 'Retailer loss', 'Consumer loss\n(avoidable)', 'Nonedible', 'Eaten')) +
+  ggtitle('Vegetable FLW by weight (LAFA)', 'Black box comprises the eaten portion plus "unavoidable" waste')
+
+ggsave('~/Dropbox/projects/foodwaste/Results/lafa_veg_flw.pdf', veg_colplot, height = 5, width = 7.5)
+
+
+# Manipulate and plot meat data -------------------------------------------
+
+# Create groups
+the_nuts <- c('Almonds', 'Coconut', 'Hazelnuts', 'Macadamia', 'Peanuts', 'Pecans', 'Pistachios', 'Walnuts', 'Other tree nuts', 'Total tree nuts')
+the_meats <- c('Beef', 'Chicken', 'Eggs', 'Lamb', 'Pork', 'Poultry', 'Red meat', 'Turkey', 'Veal')
+the_seafood <- c('Canned fish and shellfish', 'Canned Salmon', 'Canned Sardines', 'Canned shellfish', 'Canned Tuna', 'Cured fish', 'Fresh and frozen fish', 'Fresh and frozen shellfish', 'Other canned fish', 'Total fish and shellfish', 'Total Fresh and Frozen Fish')
+
+meat <- meat %>%
+  mutate(Prep_type = case_when(
+    grepl('fresh and frozen', tolower(Category)) ~ 'fresh/frozen',
+    grepl('cured', tolower(Category)) ~ 'cured'
+    grepl('fresh', tolower(Category)) ~ 'fresh',
+    grepl('canned', tolower(Category)) ~ 'canned',
+    grepl('frozen', tolower(Category)) ~ 'frozen',
+    TRUE ~ 'fresh'
+  ),
+  Food_type = case_when(
+    Category %in% the_nuts ~ 'nuts',
+    Category %in% the_meats ~ 'meats',
+    Category %in% the_seafood ~ 'seafood',
+    TRUE ~ 'all'
+  )
+  )
+
+protein_groups <- c('Red meat', 'Poultry', 'Total Fresh and Frozen Fish', 'Canned fish and shellfish', 'Total fish and shellfish', 'Total tree nuts', 'Total meat fish eggs nuts group')
+
+meat$Prep_type[meat$Category %in% c('Total fish and shellfish', 'Total meat fish eggs nuts group')] <- 'all'
+
+meat$is_group <- meat$Category %in% protein_groups
+
+meat2015 <- most_recent(meat)
+
+# Calculations for heights of stacking bars
+meat_plotdata <- meat2015 %>%
+  mutate(processor_loss = Loss_from_primary_to_retail_weight_Percent/100,
+         retailer_loss = (1 - processor_loss) * Loss_from_retail__institutional_to_consumer_level_Percent/100,
+         consumer_loss = (1 - processor_loss) * (1 - retailer_loss) * Loss_at_consumer_level_Other__cooking_loss_and_uneaten_food__Percent/100,
+         nonedible = (1 - processor_loss) * (1 - retailer_loss) * Loss_at_consumer_level_Nonedible_share_Percent/100,
+         eaten = 1 - (processor_loss + retailer_loss + consumer_loss + nonedible)) %>%
+  select(Category, Prep_type, Food_type, is_group, processor_loss, retailer_loss, consumer_loss, nonedible, eaten)
+
+# Make data to draw rectangle around the available portion.
+meat_avail <- meat_plotdata %>%
+  filter(!is_group) %>%
+  filter(complete.cases(.)) %>%
+  mutate(available = nonedible + eaten)
+
+meat_colplot <- meat_plotdata %>%
+  filter(Food_type %in% 'meats') %>%
+  filter(complete.cases(.)) %>%
+  melt(id.vars = 1:4, value.name = 'proportion') %>%
+  ggplot(aes(x = Category, y = proportion, fill = variable)) +
+  geom_col() +
+  geom_col(data = meat_avail %>% filter(Food_type %in% 'meats'), aes(y = available), fill = 'transparent', color = 'black', size = 1.5) +
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1.02), expand = c(0,0)) +
+  th_bar +
+  scale_fill_manual(name = 'Proportion', values = cols_bar, labels = c('Processor loss', 'Retailer loss', 'Consumer loss\n(avoidable)', 'Nonedible', 'Eaten')) +
+  ggtitle('Meat FLW by weight (LAFA)', 'Black box comprises the eaten portion plus "unavoidable" waste')
+
+ggsave('~/Dropbox/projects/foodwaste/Results/lafa_meat_flw.pdf', meat_colplot, height = 5, width = 6)
+
+seafood_colplot <- meat_plotdata %>%
+  filter(Food_type %in% 'seafood') %>%
+  mutate(Category = Hmisc::capitalize(tolower(Category))) %>%
+  filter(complete.cases(.)) %>%
+  melt(id.vars = 1:4, value.name = 'proportion') %>%
+  ggplot(aes(x = Category, y = proportion, fill = variable)) +
+  geom_col() +
+  geom_col(data = meat_avail %>% filter(Food_type %in% 'seafood') %>% mutate(Category = Hmisc::capitalize(tolower(Category))), aes(y = available), fill = 'transparent', color = 'black', size = 1.5) +
+  scale_x_discrete(labels = function(x) lapply(strwrap(x, width = 10, simplify = FALSE), paste, collapse="\n")) +
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1.02), expand = c(0,0)) +
+  th_bar +
+  scale_fill_manual(name = 'Proportion', values = cols_bar, labels = c('Processor loss', 'Retailer loss', 'Consumer loss\n(avoidable)', 'Nonedible', 'Eaten')) +
+  ggtitle('Seafood FLW by weight (LAFA)', 'Black box comprises the eaten portion plus "unavoidable" waste')
+
+ggsave('~/Dropbox/projects/foodwaste/Results/lafa_seafood_flw.pdf', seafood_colplot, height = 5, width = 8)
+
+nuts_colplot <- meat_plotdata %>%
+  filter(Food_type %in% 'nuts') %>%
+  filter(complete.cases(.)) %>%
+  melt(id.vars = 1:4, value.name = 'proportion') %>%
+  ggplot(aes(x = Category, y = proportion, fill = variable)) +
+  geom_col() +
+  geom_col(data = meat_avail %>% filter(Food_type %in% 'nuts'), aes(y = available), fill = 'transparent', color = 'black', size = 1.5) +
+  scale_x_discrete(labels = function(x) lapply(strwrap(x, width = 10, simplify = FALSE), paste, collapse="\n")) +
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1.02), expand = c(0,0)) +
+  th_bar +
+  scale_fill_manual(name = 'Proportion', values = cols_bar, labels = c('Processor loss', 'Retailer loss', 'Consumer loss\n(avoidable)', 'Nonedible', 'Eaten')) +
+  ggtitle('Nuts FLW by weight (LAFA)', 'Black box comprises the eaten portion plus "unavoidable" waste')
+
+ggsave('~/Dropbox/projects/foodwaste/Results/lafa_nuts_flw.pdf', nuts_colplot, height = 5, width = 8)
+
+
+# Manipulate and plot sugars ----------------------------------------------
+
+# easier, just has a few categories and no difference in prep methods.
+
+sugar_grps <- c('Total honey and syrup', 'Corn sweeteners', 'Caloric sweeteners')
+
+sugar <- sugar %>% mutate(is_group = Category %in% sugar_grps)
+
+sugar2015 <- most_recent(sugar)
+
+sugar_plotdata <- sugar2015 %>%
+  mutate(processor_loss = Loss_from_primary_to_retail_weight_Percent/100,
+         retailer_loss = (1 - processor_loss) * Loss_from_retail__institutional_to_consumer_level_Percent/100,
+         consumer_loss = (1 - processor_loss) * (1 - retailer_loss) * Loss_at_consumer_level_Other__cooking_loss_and_uneaten_food__Percent/100,
+         nonedible = (1 - processor_loss) * (1 - retailer_loss) * Loss_at_consumer_level_Nonedible_share_Percent/100,
+         eaten = 1 - (processor_loss + retailer_loss + consumer_loss + nonedible)) %>%
+  select(Category, is_group, processor_loss, retailer_loss, consumer_loss, nonedible, eaten)
+
+sugar_colplot <- sugar_plotdata %>%
+  select(-nonedible) %>%
+  filter(complete.cases(.)) %>%
+  melt(id.vars = 1:2, value.name = 'proportion') %>%
+  ggplot(aes(x = Category, y = proportion, fill = variable)) +
+  geom_col() +
+  scale_x_discrete(labels = function(x) lapply(strwrap(x, width = 12, simplify = FALSE), paste, collapse="\n")) +
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1.02), expand = c(0,0)) +
+  th_bar +
+  scale_fill_manual(name = 'Proportion', values = cols_bar[-4], labels = c('Processor loss', 'Retailer loss', 'Consumer loss\n(avoidable)', 'Eaten')) +
+  ggtitle('Sugars FLW by weight (LAFA)', 'Not considered to have nonedible portion at consumer level')
+
+ggsave('~/Dropbox/projects/foodwaste/Results/lafa_sugars_flw.pdf', sugar_colplot, height = 5, width = 7)
+
+
+# Manipulate and plot dairy data ------------------------------------------
+
+dairy <- dairy %>%
+  mutate(Prep_type = case_when(
+    grepl('evap|Evap', Category) ~ 'evaporated',
+    grepl('dry|Dry', Category) ~ 'dry',
+    grepl('Ice milk', Category) ~ 'other',
+    grepl('cheese', Category) ~ 'cheese',
+    grepl('milk', Category) ~ 'milk',
+    
+    TRUE ~ 'other'
+  )
+  )
+
+dairy_grp_names <- c('All plain milk', 'All flavored milk', 'All beverage milks', 'Total fluid milk', 'All American cheese', 'All Italian cheese', 'Total miscellaneous cheeses', 'Total cheese', 'Total cottage cheese', 'Frozen dairy products', 'All evaporated condensed milk', 'Dry milk products', 'Total dairy products', 'Half and half and eggnog')
+
+dairy <- dairy %>% mutate(is_group = Category %in% dairy_grp_names)
+
+dairy2015 <- most_recent(dairy)
+
+dairy_plotdata <- dairy2015 %>%
+  mutate(processor_loss = Loss_from_primary_to_retail_weight_Percent/100,
+         retailer_loss = (1 - processor_loss) * Loss_from_retail__institutional_to_consumer_level_Percent/100,
+         consumer_loss = (1 - processor_loss) * (1 - retailer_loss) * Loss_at_consumer_level_Other__cooking_loss_and_uneaten_food__Percent/100,
+         nonedible = (1 - processor_loss) * (1 - retailer_loss) * Loss_at_consumer_level_Nonedible_share_Percent/100,
+         eaten = 1 - (processor_loss + retailer_loss + consumer_loss + nonedible)) %>%
+  select(Category, Prep_type, is_group, processor_loss, retailer_loss, consumer_loss, nonedible, eaten)
+
+
+
+# Reduce labeling
+dairy_plotdata$Category[dairy_plotdata$Prep_type == 'milk'] <- gsub(' milk', '', dairy_plotdata$Category[dairy_plotdata$Prep_type == 'milk'])
+dairy_plotdata$Category[dairy_plotdata$Prep_type == 'cheese'] <- gsub(' cheese', '', dairy_plotdata$Category[dairy_plotdata$Prep_type == 'cheese'])
+
+# Make data to draw rectangle around the available portion.
+dairy_avail <- dairy_plotdata %>%
+  filter(!is_group) %>%
+  filter(complete.cases(.)) %>%
+  mutate(available = nonedible + eaten)
+
+
+dairy_colplot <- dairy_plotdata %>%
+  filter(!is_group) %>%
+  filter(complete.cases(.)) %>%
+  melt(id.vars = 1:3, value.name = 'proportion') %>%
+  ggplot(aes(x = Category, y = proportion, fill = variable)) +
+  facet_wrap(~ Prep_type, scales = 'free_x') +
+  geom_col() +
+  geom_col(data = dairy_avail, aes(y = available), fill = 'transparent', color = 'black', size = 1.5) +
+  scale_x_discrete(labels = function(x) lapply(strwrap(x, width = 12, simplify = FALSE), paste, collapse="\n")) +
+    scale_y_continuous(labels = scales::percent, limits = c(0, 1.02), expand = c(0,0)) +
+  th_bar +
+  scale_fill_manual(name = 'Proportion', values = cols_bar, labels = c('Processor loss', 'Retailer loss', 'Consumer loss\n(avoidable)', 'Nonedible', 'Eaten')) +
+  ggtitle('Dairy FLW by weight (LAFA)', 'Black box comprises the eaten portion plus "unavoidable" waste') +
+  theme(legend.position = c(0.8, 0.2))
+
+ggsave('~/Dropbox/projects/foodwaste/Results/lafa_dairy_flw.pdf', dairy_colplot, height = 10, width = 12)
+
+
+# Manipulate and plot fat data --------------------------------------------
+
+fat2015 <- most_recent(fat)
+
+fat_plotdata <- fat2015 %>%
+  mutate(processor_loss = Loss_from_primary_to_retail_weight_Percent/100,
+         retailer_loss = (1 - processor_loss) * Loss_from_retail__institutional_to_consumer_level_Percent/100,
+         consumer_loss = (1 - processor_loss) * (1 - retailer_loss) * Loss_at_consumer_level_Other__cooking_loss_and_uneaten_food__Percent/100,
+         nonedible = (1 - processor_loss) * (1 - retailer_loss) * Loss_at_consumer_level_Nonedible_share_Percent/100,
+         eaten = 1 - (processor_loss + retailer_loss + consumer_loss + nonedible)) %>%
+  select(Category, processor_loss, retailer_loss, consumer_loss, nonedible, eaten)
+
+fat_colplot <- fat_plotdata %>%
+  select(-nonedible) %>%
+  filter(complete.cases(.)) %>%
+  melt(id.vars = 1, value.name = 'proportion') %>%
+  ggplot(aes(x = Category, y = proportion, fill = variable)) +
+  geom_col() +
+  scale_x_discrete(labels = function(x) lapply(strwrap(x, width = 12, simplify = FALSE), paste, collapse="\n")) +
+  scale_y_continuous(labels = scales::percent, limits = c(0, 1.02), expand = c(0,0)) +
+  th_bar +
+  scale_fill_manual(name = 'Proportion', values = cols_bar[-4], labels = c('Processor loss', 'Retailer loss', 'Consumer loss\n(avoidable)', 'Eaten')) +
+  ggtitle('Fats FLW by weight (LAFA)', 'Not considered to have nonedible portion at consumer level')
+
+ggsave('~/Dropbox/projects/foodwaste/Results/lafa_fats_flw.pdf', fat_colplot, height = 5, width = 8)
--- read_data/read_cfs.r
+++ read_data/read_cfs.r
@@ -1,4 +1,54 @@
 # Look at CFS data
 
+library(dplyr)
 library(data.table)
+library(units)
+
 cfs <- fread('Q:/CFS/cfs_2012_pumf_csv.txt')
+
+# calculate total values
+cfs <- cfs %>%
+  mutate(total_value = SHIPMT_VALUE * WGT_FACTOR,
+         total_weight = SHIPMT_WGHT * WGT_FACTOR %>% set_units(lb) %>% set_units(kg) %>% as.numeric)
+
+#### Create state by state matrices for total value and total weight
+
+# First sum up by origin and destination.
+cfs_totals <- cfs %>%
+  group_by(ORIG_STATE, DEST_STATE) %>%
+  summarize(value = sum(total_value), mass = sum(total_weight))
+# Note: a few have originating state of zero for confidentiality reasons but I guess we should ignore this?
+
+sum(cfs_totals$value[cfs_totals$ORIG_STATE == 0])/sum(cfs_totals$value) # A very tiny proportion (less than 0.01%)
+sum(cfs_totals$mass[cfs_totals$ORIG_STATE == 0])/sum(cfs_totals$mass)
+
+# Next convert to square matrix.
+# This can now be done with tidyr instead of reshape2 but probably about the same for each.
+
+totals2square <- function(dat, x_col, y_col, value_col) {
+  x_col <- enquo(x_col)
+  y_col <- enquo(y_col)
+  value_col <- enquo(value_col)
+  mat <- dat %>%
+    select(!!x_col, !!y_col, !!value_col) %>%
+    spread(!!y_col, !!value_col) %>%
+    as.matrix
+  dimnames(mat)[[1]] <- mat[,1]
+  mat[is.na(mat)] <- 0
+  mat[,-1]
+}
+
+value_mat <- cfs_totals %>%
+  filter(ORIG_STATE != 0) %>%
+  totals2square(ORIG_STATE, DEST_STATE, value)
+
+mass_mat <- cfs_totals %>%
+  filter(ORIG_STATE != 0) %>%
+  totals2square(ORIG_STATE, DEST_STATE, mass)
+
+library(circlize)
+chordDiagram(value_mat)
+
+# Pull out some of the codes that have to do with the food system
+# 01 live animals, 02 cereal grains, 03 other ag products, 04 animal feed, eggs, honey, other animal products, 05 meat 
+# 06 milled grain and breads, 07 other prepared food, oil 08 alcoholic beverages
