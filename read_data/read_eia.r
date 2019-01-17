c26816481728fe0ac1ff0115c4360d19240a73da
Quentin Read <qread@rstudio03.research.sesync.org>
2019-01-17 08:49:50 -0500

    Update read_eia.r

--- read_data/read_eia.r
+++ read_data/read_eia.r
@@ -8,4 +8,10 @@ eia_list <- lapply(1:10, function(i) {
 })
 
 # HC1: fuels used and end uses
+hc1.1 <- readWorksheetFromFile('/nfs/qread-data/EIA/hc1.1.xlsx', sheet = 1)
+hc2.1 <- readWorksheetFromFile('/nfs/qread-data/EIA/hc2.1.xlsx', sheet = 1)
+hc1.2 <- readWorksheetFromFile('/nfs/qread-data/EIA/hc1.2.xlsx', sheet = 1)
 
+num_rows <- apply(hc1.1, 1, function(x) any(!is.na(as.numeric(x))))
+hc1.1_numeric <- hc1.1[num_rows,]
+hc1.1[!num_rows, 1]
