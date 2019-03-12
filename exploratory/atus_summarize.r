24bae820418c82918b888d3d19e71e5fa8af6266
Quentin Read <quentin.read@gmail.com>
2019-03-12 15:53:28 -0400

    Write atus barplots to a file

--- exploratory/atus_summarize.r
+++ exploratory/atus_summarize.r
@@ -129,3 +129,225 @@ atus_sums_weighted_race <- atus_sums %>% tabulate_atus(race)
 atus_sums_weighted_famincome <- atus_sums %>% tabulate_atus(famincome)
 
 save(list = ls(pattern = 'atus_sums_agg|atus_sums_weighted'), file = file.path(fp, 'ATUS/final_data/sums_by_group.RData'))
+
+
+# Plot by demographic group -----------------------------------------------
+
+load(file.path(fp, 'ATUS/final_data/sums_by_group.RData'))
+
+# Overall - major categories
+atus_sums_weighted %>%
+  group_by(major_category) %>%
+  summarize(dur = sum(dur)) %>%
+ggplot() +
+  geom_col(aes(x = 1, y = dur, fill = major_category), position = 'stack') +
+  scale_x_continuous(limits = c(0.3, 1.7)) +
+  scale_y_continuous(name = "Time (min)", limits = c(0, 1440), expand = c(0,0)) +
+  theme_bw() +
+  coord_flip() +
+  ggtitle('How the average American\'s day is spent', 'source: American Time Use Survey') +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.title.x = element_blank())
+
+# Make more of a table with this.
+write.csv(atus_sums_weighted, file = '~/google_drive/SESYNC Food Waste/Results_Figures/atus_sums.csv', row.names = FALSE)
+
+# Important number
+atus_sums_weighted %>% filter(major_category == 'traveling')
+
+ggplot(atus_sums_weighted %>% filter(!food_activity_group %in% 'nonfood')) +
+  geom_col(aes(x = 1, y = dur, fill = food_activity_group), position = 'stack') +
+  scale_x_continuous(limits = c(0.3, 1.7)) +
+  scale_y_continuous(name = "Time (min)", limits = c(0, 130), expand = c(0,0)) +
+  scale_fill_discrete(name = 'Activity') +
+  theme_bw() +
+  ggtitle('How are the 2 hours spent on food divided?', 'source: American Time Use Survey') +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.title.x = element_blank())
+ggsave('~/google_drive/SESYNC Food Waste/Results_Figures/atus_foodtime.png', height = 5, width = 6)
+
+# Distinguish the different types of travel
+atus_sums_weighted %>% filter(major_category == 'traveling') %>% mutate(prop = dur/sum(dur))
+# 9.6% travel for eating at restaurants, 4.5% travel for grocery shopping.
+
+# Plot over time
+brewfill <- scale_fill_brewer(type = 'qual', palette = 'Set1', name = 'Activity', labels = c('Travel to eat and drink outside the home', 'Travel to grocery shopping', 'Other'))
+
+yearstacked <- atus_sums_weighted_year %>% 
+  filter(major_category %in% 'traveling', !food_activity_name %in% 'eating_drinking_travel_nonspecified') %>%
+  group_by(tuyear, food_activity_name) %>%
+  summarize(dur = sum(dur)) %>%
+  ggplot() +
+    geom_col(aes(x = tuyear, y = dur, fill = food_activity_name), position = 'stack') +
+    scale_x_continuous(name = 'Year', labels = 2003:2016, breaks = 2003:2016) +
+    scale_y_continuous(name = "Time (min)", limits = c(0, 80), expand = c(0,0)) +
+    brewfill + 
+    theme_bw() +
+    theme(legend.position = 'bottom') +
+    ggtitle('Proportion of travel related to food', 'source: American Time Use Survey') 
+
+sexstacked <- atus_sums_weighted_sex %>% 
+  filter(major_category %in% 'traveling', !food_activity_name %in% 'eating_drinking_travel_nonspecified') %>%
+  group_by(sex, food_activity_name) %>%
+  summarize(dur = sum(dur)) %>%
+  ggplot() +
+  geom_col(aes(x = sex, y = dur, fill = food_activity_name), position = 'stack') +
+  scale_y_continuous(name = "Time (min)", limits = c(0, 80), expand = c(0,0)) +
+  brewfill + 
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  ggtitle('Proportion of travel related to food', 'source: American Time Use Survey') 
+
+racestacked <- atus_sums_weighted_race %>% 
+  filter(major_category %in% 'traveling', !food_activity_name %in% 'eating_drinking_travel_nonspecified') %>%
+  group_by(race, food_activity_name) %>%
+  summarize(dur = sum(dur)) %>%
+  ggplot() +
+  geom_col(aes(x = race, y = dur, fill = food_activity_name), position = 'stack') +
+  scale_y_continuous(name = "Time (min)", limits = c(0, 80), expand = c(0,0)) +
+  scale_fill_brewer(type = 'qual', palette = 'Set1') +
+  brewfill + 
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  ggtitle('Proportion of travel related to food', 'source: American Time Use Survey') 
+
+agestacked <- atus_sums_weighted_age %>% 
+  filter(major_category %in% 'traveling', !food_activity_name %in% 'eating_drinking_travel_nonspecified') %>%
+  group_by(age_cat, food_activity_name) %>%
+  summarize(dur = sum(dur)) %>%
+  ggplot() +
+  geom_col(aes(x = age_cat, y = dur, fill = food_activity_name), position = 'stack') +
+  scale_x_discrete(name = 'Age Group', labels = c('15-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+')) +
+  scale_y_continuous(name = "Time (min)", limits = c(0, 80), expand = c(0,0)) +
+  brewfill + 
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  ggtitle('Proportion of travel related to food', 'source: American Time Use Survey') 
+
+incomestacked <- atus_sums_weighted_famincome %>% 
+  filter(major_category %in% 'traveling', !food_activity_name %in% 'eating_drinking_travel_nonspecified', !is.na(famincome)) %>%
+  group_by(famincome, food_activity_name) %>%
+  summarize(dur = sum(dur)) %>%
+  ggplot() +
+  geom_col(aes(x = famincome, y = dur, fill = food_activity_name), position = 'stack') +
+  scale_x_discrete(name = 'Household income') +
+  scale_y_continuous(name = "Time (min)", limits = c(0, 90), expand = c(0,0)) +
+  brewfill + 
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  ggtitle('Proportion of travel related to food', 'source: American Time Use Survey') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+regionstacked <- atus_sums_weighted_region %>% 
+  filter(major_category %in% 'traveling', !food_activity_name %in% 'eating_drinking_travel_nonspecified') %>%
+  group_by(region, food_activity_name) %>%
+  summarize(dur = sum(dur)) %>%
+  ggplot() +
+  geom_col(aes(x = region, y = dur, fill = food_activity_name), position = 'stack') +
+  scale_y_continuous(name = "Time (min)", limits = c(0, 80), expand = c(0,0)) +
+  brewfill + 
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  ggtitle('Proportion of travel related to food', 'source: American Time Use Survey') 
+
+# Same graphs as percentages ----------------------------------------------
+
+yearpct <- atus_sums_weighted_year %>% 
+  filter(major_category %in% 'traveling', !food_activity_name %in% 'eating_drinking_travel_nonspecified') %>%
+  group_by(tuyear, food_activity_name) %>%
+  summarize(dur = sum(dur)) %>%
+  group_by(tuyear) %>%
+  mutate(prop = dur/sum(dur)) %>%
+  filter(!food_activity_name %in% 'nonfood') %>%
+  ggplot() +
+  geom_col(aes(x = tuyear, y = prop, fill = food_activity_name), position = 'dodge') +
+  scale_x_continuous(name = 'Year', labels = 2003:2016, breaks = 2003:2016) +
+  scale_y_continuous(name = "Proportion", limits = c(0, 0.11), expand = c(0,0)) +
+  brewfill + 
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  ggtitle('Proportion of travel related to food', 'source: American Time Use Survey') 
+
+sexpct <- atus_sums_weighted_sex %>% 
+  filter(major_category %in% 'traveling', !food_activity_name %in% 'eating_drinking_travel_nonspecified') %>%
+  group_by(sex, food_activity_name) %>%
+  summarize(dur = sum(dur)) %>%
+  group_by(sex) %>%
+  mutate(prop = dur/sum(dur)) %>%
+  filter(!food_activity_name %in% 'nonfood') %>%
+  ggplot() +
+  geom_col(aes(x = sex, y = prop, fill = food_activity_name), position = 'dodge') +
+  scale_y_continuous(name = "Proportion", limits = c(0, 0.11), expand = c(0,0)) +
+  brewfill + 
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  ggtitle('Proportion of travel related to food', 'source: American Time Use Survey') 
+
+racepct <- atus_sums_weighted_race %>% 
+  filter(major_category %in% 'traveling', !food_activity_name %in% 'eating_drinking_travel_nonspecified') %>%
+  group_by(race, food_activity_name) %>%
+  summarize(dur = sum(dur)) %>%
+  group_by(race) %>%
+  mutate(prop = dur/sum(dur)) %>%
+  filter(!food_activity_name %in% 'nonfood') %>%
+  ggplot() +
+  geom_col(aes(x = race, y = prop, fill = food_activity_name), position = 'dodge') +
+  scale_y_continuous(name = "Proportion", limits = c(0, 0.11), expand = c(0,0)) +
+  brewfill + 
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  ggtitle('Proportion of travel related to food', 'source: American Time Use Survey') 
+
+agepct <- atus_sums_weighted_age %>% 
+  filter(major_category %in% 'traveling', !food_activity_name %in% 'eating_drinking_travel_nonspecified') %>%
+  group_by(age_cat, food_activity_name) %>%
+  summarize(dur = sum(dur)) %>%
+  group_by(age_cat) %>%
+  mutate(prop = dur/sum(dur)) %>%
+  filter(!food_activity_name %in% 'nonfood') %>%
+  ggplot() +
+  geom_col(aes(x = age_cat, y = prop, fill = food_activity_name), position = 'dodge') +
+  scale_x_discrete(name = 'Age Group', labels = c('15-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+')) +
+  scale_y_continuous(name = "Proportion", limits = c(0, 0.2), expand = c(0,0)) +
+  brewfill + 
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  ggtitle('Proportion of travel related to food', 'source: American Time Use Survey') 
+
+incomepct <- atus_sums_weighted_famincome %>% 
+  filter(major_category %in% 'traveling', !food_activity_name %in% 'eating_drinking_travel_nonspecified', !is.na(famincome)) %>%
+  group_by(famincome, food_activity_name) %>%
+  summarize(dur = sum(dur)) %>%
+  group_by(famincome) %>%
+  mutate(prop = dur/sum(dur)) %>%
+  filter(!food_activity_name %in% 'nonfood') %>%
+  ggplot() +
+  geom_col(aes(x = famincome, y = prop, fill = food_activity_name), position = 'dodge') +
+  scale_x_discrete(name = 'Household income') +
+  scale_y_continuous(name = "Proportion", limits = c(0, 0.12), expand = c(0,0)) +
+  brewfill + 
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  ggtitle('Proportion of travel related to food', 'source: American Time Use Survey') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+regionpct <- atus_sums_weighted_region %>% 
+  filter(major_category %in% 'traveling', !food_activity_name %in% 'eating_drinking_travel_nonspecified') %>%
+  group_by(region, food_activity_name) %>%
+  summarize(dur = sum(dur)) %>%
+  group_by(region) %>%
+  mutate(prop = dur/sum(dur)) %>%
+  filter(!food_activity_name %in% 'nonfood') %>%
+  ggplot() +
+  geom_col(aes(x = region, y = prop, fill = food_activity_name), position = 'dodge') +
+  scale_y_continuous(name = "Proportion", limits = c(0, 0.11), expand = c(0,0)) +
+  brewfill + 
+  theme_bw() +
+  theme(legend.position = 'bottom') +
+  ggtitle('Proportion of travel related to food', 'source: American Time Use Survey') 
+
+pdf('~/google_drive/SESYNC Food Waste/Results_Figures/atus_stackedbarplots.pdf', height = 5, width = 7)
+yearstacked; sexstacked; agestacked; racestacked; incomestacked; regionstacked
+dev.off()
+
+pdf('~/google_drive/SESYNC Food Waste/Results_Figures/atus_proportionbarplots.pdf', height = 5, width = 7)
+yearpct; sexpct; agepct; racepct; incomepct; regionpct
+dev.off()
