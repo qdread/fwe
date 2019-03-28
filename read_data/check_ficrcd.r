9566bbe63df610e541526af2f29c12e93dec697d
Quentin Read <qread@rstudio03.research.sesync.org>
2019-03-28 14:32:36 -0400

    Correct ficrcd 2008 to add all necessary 'other' columns

--- read_data/check_ficrcd.r
+++ read_data/check_ficrcd.r
@@ -1,6 +1,8 @@
 # read FICRCD to see if columns match
 
-fic08 <- read.csv('Z:/FICRCD/ficrcd_2007_2008.csv', stringsAsFactors = FALSE)
+fp <- ifelse(dir.exists('Z:/'), 'Z:', '/nfs/fwe-data')
+
+fic08 <- read.csv(file.path(fp, 'food_consumption/FICRCD/ficrcd_2007_2008.csv'), stringsAsFactors = FALSE)
 
 # do individual vegetables add up to the total vegetables column
 with(fic08, table(TotalBrassica >= BroccoliAndCauliflower))
@@ -25,27 +27,54 @@ fic08 %>%
   pull(diff) %>%
   mean
 
+# Other grain needed
 fic08 %>%
   mutate(grain_sum = CornFlour + OatFlour + RiceDried + WheatFlour,
          diff = TotalGrain - grain_sum) %>%
   pull(diff) %>%
   mean
 
+# apples OK
 fic08 %>%
   mutate(apple_sum = Apples+ApplesFromJuice,
-         diff = TotalGrain - apple_sum) %>%
+         diff = TotalApples - apple_sum) %>%
   pull(diff) %>%
   mean
+
+# oranges OK
+fic08 %>%
+  mutate(orange_sum = Oranges+OrangesFromJuice,
+         diff = TotalOranges - orange_sum) %>%
+  pull(diff) %>%
+  mean
+
+# veg in general
+fic08 %>%
+  mutate(veg_sum = TotalBrassica + TotalLeafyVeg + TotalRootsAndTubers,
+         diff = TotalVegetables - veg_sum) %>%
+  pull(diff) %>%
+  mean
+
+
 
 # There is no "Other" column for Brassica, leafy vegetables, roots and tubers, and vegetables in general.
 
 library(tidyverse)
-fic08 %>%
-  mutate(OtherBrassica = TotalBrassica - BroccoliAndCauliflower,
+fic08_modified <- fic08 %>%
+  mutate(OtherVegetables = TotalVegetables - TotalBrassica - TotalLeafyVeg - TotalRootsAndTubers,
+         OtherBrassica = TotalBrassica - BroccoliAndCauliflower,
          OtherLeafyVeg = TotalLeafyVeg - Lettuce,
          OtherRootsAndTubers = TotalRootsAndTubers - Potatoes,
          OtherPoultry = TotalPoultry - (Chicken + Turkey),
-         OtherMeat = TotalMeat - (Beef + Pork + TotalPoultry + FinAndShellfish),
+         OtherMeat = TotalMeatPoultryFish - (Beef + Pork + TotalPoultry + FinAndShellfish),
          OtherNuts = TotalNuts - (Peanuts + TreeNuts),
          OtherGrain = TotalGrain - (CornFlour + OatFlour + RiceDried + WheatFlour),
-         OtherFruit = )
+         OtherFruit = TotalFruit - (TotalApples + Bananas + Berries + Grapes + Melons + TotalOranges + OtherCitrusFruits + StoneFruits + TropicalFruits))
+
+# Find issues.
+sapply(fic08_modified, min)
+# All approx zero except for rounding error.
+fic08_modified <- fic08_modified %>%
+  mutate_at(vars(TotalDairy:OtherFruit), ~ if_else(.x < 0, 0.0, as.numeric(.x)))
+
+write.csv(fic08_modified, file.path(fp, 'food_consumption/FICRCD/corrected_ficrcd_2007_2008.csv'), row.names = FALSE)
