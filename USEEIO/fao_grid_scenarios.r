f0a32ce3dbf5e33110ea4313a2c14cca3520e340
Quentin Read <qread@rstudio03.research.sesync.org>
2019-05-06 15:20:43 -0400

    Rewrite pipeline for grid scenario to be runnable in parallel!

--- USEEIO/fao_grid_scenarios.r
+++ USEEIO/fao_grid_scenarios.r
@@ -1,5 +1,7 @@
 # "Grid" scenarios with many possible combinations of waste reduction by 1% increments up to 50% for each sector.
 # using baseline loss rates from FAO and using the corrected way of modifying and renormalizing the make and use tables
+# Parallelism implemented to speed up the jobs on Rstudio server
+# Rewrite entire pipeline so that the temporary files are created and deleted within each task, preventing buildup of lots of files.
 # QDR / FWE / 06 May 2019
 
 
@@ -9,12 +11,13 @@
 library(tidyverse)
 library(XLConnect)
 library(reticulate)
+library(furrr)
 
 fp_github <- ifelse(dir.exists('~/Documents/GitHub'), '~/Documents/GitHub/fwe', '/research-home/qread/fwe')
 fp_crosswalks <- file.path(ifelse(dir.exists('Q:/'), 'Q:', '/nfs/qread-data'), 'crossreference_tables')
 fp_scenario <- file.path(ifelse(dir.exists('Q:/'), 'Q:', '/nfs/qread-data'), 'scenario_inputdata')
 fp_bea <- file.path(ifelse(dir.exists('Z:/'), 'Z:', '/nfs/fwe-data'), 'BEA/formatted')
-fp_temp <- ifelse(dir.exists('~/Documents/temp'), '~/Documents/temp', '/nfs/qread-data/temp')
+fp_temp <- ifelse(dir.exists('~/Documents/temp'), '~/Documents/temp', '/tmp')
 fp_output <- file.path(ifelse(dir.exists('Q:/'), 'Q:', '/nfs/qread-data'), 'scenario_results')
 fp_useeio <- ifelse(dir.exists('~/Dropbox'), '~/Dropbox/projects/foodwaste/Code/USEEIO-master', '/research-home/qread/USEEIO')
 
@@ -65,91 +68,88 @@ baseline_waste_rate <- map2_dbl(FAO_category_vectors, 1:nrow(naics_foodsystem), 
 # p is proportion of the sector representing the food system
 demand_change_fn <- function(w_orig, r, p) p * ((1 - w_orig) / (1 - (1 - r) * w_orig) - 1) + 1
 
-# Scenarios are waste reduction across 0-3 sectors
-# We calculate the new demand as a fraction of the original demand, accounting for the sectors that are only partially food
-reduction_rates <- e1071::bincombinations(3) 
-reduction_rates <- 1 - (0.75 ^ sweep(reduction_rates, 1, apply(reduction_rates, 1, sum), '/'))
-colnames(reduction_rates) <- c('L1', 'L2', 'L3')
-reduction_rates[is.na(reduction_rates)] <- 0
+# In this version, we will run the full grid combination of reduction rates
+# Only do 0.05 increment for now
+rs <- seq(0, 1, by = 0.05)
+reduction_rates <- expand.grid(L1 = rs, L2 = rs, L3 = rs) # Over 9000
 
 demand_change_factors <- apply(reduction_rates, 1, function(v) {
   reduction_rate_bysector <- v[naics_foodsystem$stage_code]
   demand_change_fn(baseline_waste_rate, reduction_rate_bysector, naics_foodsystem$proportion_food)
 })
 
-# Give the scenarios names based on which levels are reduced.
-scenario_names <- c('baseline', apply(reduction_rates, 1, function(v) paste(names(v)[v>0], collapse = ''))[-1])
-colnames(demand_change_factors) <- paste0('demandchange_', scenario_names)
+# Give the scenarios names - in this case just numbers
+scenario_names <- paste0('demandchange_', 1:ncol(demand_change_factors))
+colnames(demand_change_factors) <- scenario_names
 
 loss_rates <- cbind(naics_foodsystem, demand_change_factors)
 
-# Step 3: Create modified make and use tables for each scenario -----------
 
-# Load make and use tables
-M <- read.csv(file.path(fp_bea, 'make2012.csv'), row.names = 1, check.names = FALSE)
-U <- read.csv(file.path(fp_bea, 'use2012.csv'), row.names = 1, check.names = FALSE)
+# Step 2B: Setup steps before parallelized part ---------------------------
 
-MU_modified <- map(loss_rates %>% select(starts_with('demandchange')), ~ modify_and_renormalize_make_and_use(M, U, .x, loss_rates$BEA_389_code))
-
-# Write the make and use tables to CSVs
-iwalk(MU_modified, ~ write.csv(.x$M, file = file.path(fp_temp, paste0('make_', .y, '.csv'))))
-iwalk(MU_modified, ~ write.csv(.x$U, file = file.path(fp_temp, paste0('use_', .y, '.csv'))))
-
-
-# Step 4: Build USEEIO with each of the make and use tables ---------------
-
-# (Write the resulting model build files to a directory.)
-
-source(file.path(fp_useeio, 'R/Model Build Scripts/USEEIO2012_buildfunction.R'))
-model_build_path <- file.path(fp_useeio, 'useeiopy/Model Builds')
-
-# Walk through the list of scenario names and build the model with the appropriate make and use tables.
-# Note: it takes a few seconds to build each model.
-walk(scenario_names, ~ build_USEEIO(outputfolder = file.path(model_build_path, paste0('scenario_', .x)),
-                                    model = paste0('scenario_', .x),
-                                    usetablefile = file.path(fp_temp, paste0('use_demandchange_', .x, '.csv')),
-                                    maketablefile = file.path(fp_temp, paste0('make_demandchange_', .x, '.csv')),
-                                    code_path = fp_useeio
-                                    )
-     )
-
-
-# Step 5: Extract demand vectors for food system from each scenario -------
-
-# Read final demand CSV from each built model.
-all_final_demand <- map(scenario_names, ~ read.csv(file.path(model_build_path, paste0('scenario_', .x), paste0('scenario_', .x, '_FinalDemand.csv')), stringsAsFactors = FALSE))
-all_codes <- read.csv(file.path(fp_crosswalks, 'all_codes.csv'), stringsAsFactors = FALSE)
-
-# Join each full final demand vector with food system proportions 
-# Also match these with the correct codes that have full description names
-all_final_demand <- map(all_final_demand, ~ .x %>%
-                          left_join(naics_foodsystem) %>% 
-                          filter(!is.na(proportion_food)) %>%
-                          left_join(all_codes, by = c('BEA_389_code' = 'sector_code_uppercase')))
-
-# Convert each demand vector to separate lists of codes and values
-final_demand_lists <- map(all_final_demand, ~ list(codes = as.list(.x$sector_desc_drc), values = as.list(.x$X2012_US_Consumption * .x$proportion_food)))
-
-
-# Step 6. Run the models! -------------------------------------------------
+# Set up parallel jobs
+plan(multiprocess(workers = 8))
 
 # Source Python script which runs model
 # If run on server, specify we're using python3
 if (dir.exists('/nfs/fwe-data')) use_python('/usr/bin/python3')
 source_python(file.path(fp_github, 'USEEIO/eeio_lcia.py'))
 
-# Run all scenarios. (takes a couple seconds per scenario)
-eeio_result <- map2(final_demand_lists, scenario_names, ~ eeio_lcia(paste0('scenario_', .y), .x$values, .x$codes))
-impact_categories <- rownames(eeio_result[[1]])
-eeio_result <- map2_dfr(eeio_result, scenario_names, ~ data.frame(scenario = .y, .x))
-eeio_result <- with(eeio_result, data.frame(scenario, impact_category = impact_categories, value = Total, stringsAsFactors = FALSE))
+# Source R script which builds models
+source(file.path(fp_useeio, 'R/Model Build Scripts/USEEIO2012_buildfunction.R'))
+model_build_path <- file.path(fp_useeio, 'useeiopy/Model Builds')
 
+# Load make and use tables
+M <- read.csv(file.path(fp_bea, 'make2012.csv'), row.names = 1, check.names = FALSE)
+U <- read.csv(file.path(fp_bea, 'use2012.csv'), row.names = 1, check.names = FALSE)
 
-# Step 7. Write result to file and delete model files ---------------------
+# Load demand codes
+all_codes <- read.csv(file.path(fp_crosswalks, 'all_codes.csv'), stringsAsFactors = FALSE)
 
-write.csv(eeio_result, file.path(fp_output, 'fao_scenario_lcia_results.csv'), row.names = FALSE)
+if (!dir.exists(file.path(fp_temp, 'fwe'))) dir.create(file.path(fp_temp, 'fwe'))
+fp_mu <- file.path(fp_temp, 'fwe')
 
-# Delete the intermediate files (edited use tables, edited make tables, and model build folder) 
-invisible(file.remove(c(file.path(fp_temp, paste0('use_demandchange_', scenario_names, '.csv')),
-                        file.path(fp_temp, paste0('make_demandchange_', scenario_names, '.csv')))))
-unlink(file.path(model_build_path, paste0('scenario_', scenario_names)), recursive = TRUE)
+# ====================================== #
+# Steps 3 through 7 are run in parallel  #
+# ====================================== #
+
+# Master function to run steps 3 through 7
+# v is the demand vector and i is the name of the scenario
+
+get_eeio_result <- function(v, i) {
+  # Step 3. Create modified make and use tables for each scenario and write them to CSVs.
+  MU_modified <- modify_and_renormalize_make_and_use(M, U, v, loss_rates$BEA_389_code)
+  write.csv(MU_modified$M, file = file.path(fp_mu, paste0('make_', i, '.csv')))
+  write.csv(MU_modified$U, file = file.path(fp_mu, paste0('use_', i, '.csv')))
+  # Step 4. Build USEEIO with those make and use tables
+  build_USEEIO(outputfolder = file.path(model_build_path, paste0('scenario_', i)),
+               model = paste0('scenario_', i),
+               usetablefile = file.path(fp_mu, paste0('use_', i, '.csv')),
+               maketablefile = file.path(fp_mu, paste0('make_', i, '.csv')),
+               code_path = fp_useeio
+  )
+  # Step 5. Extract demand vector for food system from scenario.
+  # Join this with the food system proportions and with the correct demand codes (full description names)
+  all_final_demand <- read.csv(file.path(model_build_path, paste0('scenario_', i), paste0('scenario_', i, '_FinalDemand.csv')), stringsAsFactors = FALSE) %>%
+    left_join(naics_foodsystem) %>% 
+    filter(!is.na(proportion_food)) %>%
+    left_join(all_codes, by = c('BEA_389_code' = 'sector_code_uppercase'))
+  # Convert demand vector to separate list of codes and values
+  final_demand_list <- with(all_final_demand, list(codes = as.list(sector_desc_drc), values = as.list(X2012_US_Consumption * proportion_food)))
+  # Step 6. Run the model!
+  eeio_result <- eeio_lcia(paste0('scenario_', i), final_demand_list$values, final_demand_list$codes)
+  eeio_result <- data.frame(scenario = i, impact_category = rownames(eeio_result), value = eeio_result$Total, stringsAsFactors = FALSE)
+  # Step 7. Delete the intermediate files (edited use tables, edited make tables, and model build folder) 
+  invisible(file.remove(c(file.path(fp_mu, paste0('use_', i, '.csv')),
+                          file.path(fp_mu, paste0('make_', i, '.csv')))))
+  unlink(file.path(model_build_path, paste0('scenario_', i)), recursive = TRUE)
+  
+  return(eeio_result)
+}
+
+eeio_result <- future_imap_dfr(loss_rates %>% select(starts_with('demandchange')), get_eeio_result, .progress = TRUE)
+
+# Step 8. Write result to file -----------------------------------------------
+
+write.csv(eeio_result, file.path(fp_output, 'fao_grid_scenario_lcia_results.csv'), row.names = FALSE)
+
