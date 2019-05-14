9d4f63cacfcaea32b4ec264321bc051feb8a7164
Quentin Read <qread@rstudio03.research.sesync.org>
2019-05-13 21:44:31 -0400

    Refine test optimization and plot results

--- USEEIO/testnloptimization.r
+++ USEEIO/testnloptimization.r
@@ -20,9 +20,9 @@ demand_change_fn <- function(W0, r, p) p * ((1 - W0) / (1 - (1 - r) * W0) - 1) +
 
 # Exponential or Richards curves for new waste rate given cost x, old waste rate W0, unavoidable waste rate Wu, and parameters B and nu
 waste_rate_by_cost <- function(x, W0, Wu, B, nu) {
-  # If nu = 1 this becomes the exponential curve
+  # If nu = 1, this becomes the exponential curve
   A <- (W0 - (2 ^ (-1/nu)) * Wu) / (1 - 2 ^ (-1/nu))
-  A + (Wu - A)/((1 + exp(-B*x))^(1/nu))
+  pmin(W0, A + (Wu - A)/((1 + exp(-B*x))^(1/nu)))
 }
 
 # function to calculate the waste rates for all sectors, given the costs for the 3 stages, and the parameters described above
@@ -91,11 +91,11 @@ source(file.path(fp_github, 'USEEIO/load_scenario_data.r'))
 # fake unavoidable waste rate -- assume 25% of food waste is unavoidable
 fake_unavoidable_waste_rate <- baseline_waste_rate * 0.25
 
-# B and nu by stage
-# Assume faster returns on investment for level 2
-# Assume higher startup costs for level 1
-B_stages <- c(L1 = 0.002, L2 = 0.005, L3 = 0.002)
-nu_stages <- c(L1 = 0.1, L2 = 0.5, L3 = 1)
+# Parameters by stage
+# Assume faster returns on investment for level 1 and level 2
+# Assume higher startup costs for level 1, intermediate for level 2, none for level 3
+B_stages <- c(L1 = 0.005, L2 = 0.005, L3 = 0.002)
+nu_stages <- c(L1 = 0.1, L2 = 0.2, L3 = 1)
 
 B_sectors <- B_stages[naics_foodsystem$stage_code]
 nu_sectors <- nu_stages[naics_foodsystem$stage_code]
@@ -110,6 +110,22 @@ proportion_gross_outputs <- gross_outputs / gross_outputs_by_stage[naics_foodsys
 # Constraint: total cost - pulled out of thin air.
 Ctotal <- 1000
 
+# Plot the different curves. (added 13 May)
+# Generate the data.
+params_for_curves <- data.frame(W0 = baseline_waste_rate, Wu = fake_unavoidable_waste_rate, B = B_sectors, nu = nu_sectors)
+yvals_curves <- pmap(params_for_curves, waste_rate_by_cost, x = seq(0, 1000, 1))
+dat_for_curves <- imap_dfr(yvals_curves, ~ data.frame(sector_name = naics_foodsystem$BEA_389_def[.y], stage = naics_foodsystem$stage_code[.y], params_for_curves[.y, ], x = seq(0, 1000, 1), y = .x))
+
+cs <- RColorBrewer::brewer.pal(3, 'Set3')
+
+ggplot(dat_for_curves, aes(x = x, y = y, color = stage, group = sector_name)) +
+  geom_line() +
+  theme_bw() +
+  scale_color_brewer(type='qual', palette='Set2', labels = c('production', 'retail', 'consumption')) +
+  scale_x_continuous(name = 'Cost (relative units)', expand = c(0,0)) +
+  scale_y_continuous(name = 'Waste rate', expand = c(0,0), labels = scales::percent, limits = c(0, 0.26)) +
+  ggtitle('\"Fake\" cost curves for each FSC sector', 'Each stage has multiple sectors')
+ggsave('/nfs/qread-data/figures/costcurve_fake_example.png', height = 6, width = 6, dpi = 300)
 
 # Run optimization --------------------------------------------------------
 
@@ -149,12 +165,66 @@ res2 <- solnl(X = x0, objfun = eval_f_eeio, confun = eval_eq_totalcost_ceq, lb =
 
 res3 <- solnp(pars = x0, fun = eval_f_eeio, eqfun = eval_eq_total, eqB = Ctotal, LB = lb, UB = ub, category = ghg_name, W0_sectors = baseline_waste_rate, Wu_sectors = fake_unavoidable_waste_rate, B_sectors = B_sectors, nu_sectors = nu_sectors, p_sectors = proportion_gross_outputs, sector_stage_codes = naics_foodsystem$stage_code, Ctotal = Ctotal)
 
+# Do with several total cost amounts
+Ctotal_vec <- c(500, 1000, 2000, 5000)
+optim_bytotals <- map(Ctotal_vec, ~ solnp(pars = rep(.x/3, 3), fun = eval_f_eeio, eqfun = eval_eq_total, eqB = .x, LB = lb, UB = ub, category = ghg_name, W0_sectors = baseline_waste_rate, Wu_sectors = fake_unavoidable_waste_rate, B_sectors = B_sectors, nu_sectors = nu_sectors, p_sectors = proportion_gross_outputs, sector_stage_codes = naics_foodsystem$stage_code, Ctotal = .x))
+
 # What are the final waste rates?
-xfinal <- setNames(res3$pars, c('L1', 'L2', 'L3'))
-optimal_waste_rate <- waste_rate_by_cost(x = xfinal[sector_stage_codes], W0 = baseline_waste_rate, Wu = fake_unavoidable_waste_rate, B = B_sectors, nu = nu_sectors)
+xfinal <- map(optim_bytotals, ~ setNames(.x$pars, c('L1', 'L2', 'L3')))
+optimal_waste_rate <- map(xfinal, ~ waste_rate_by_cost(x = .x[naics_foodsystem$stage_code], W0 = baseline_waste_rate, Wu = fake_unavoidable_waste_rate, B = B_sectors, nu = nu_sectors))
+optimal_waste_rate <- map2_dfr(optimal_waste_rate, Ctotal_vec, ~ data.frame(waste_rate = .x, total_cost = .y))
 
 # Graph all.
-waste_rate_df <- data.frame(name = naics_foodsystem$BEA_389_def, stage = sector_stage_codes, baseline = baseline_waste_rate, unavoidable = fake_unavoidable_waste_rate, optimal = optimal_waste_rate) %>%
-  gather(scenario, waste_rate, -name, -stage)
+waste_rate_df <- data.frame(name = naics_foodsystem$BEA_389_def, stage = naics_foodsystem$stage_code, waste_rate = c(baseline_waste_rate, optimal_waste_rate$waste_rate), total_cost = c(rep(0, nrow(naics_foodsystem)), optimal_waste_rate$total_cost)) 
 
-ggplot(waste_rate_df, aes())
+# waste_rate_df <- data.frame(name = naics_foodsystem$BEA_389_def, stage = naics_foodsystem$stage_code, baseline = baseline_waste_rate, unavoidable = fake_unavoidable_waste_rate, optimal = optimal_waste_rate) %>%
+#   gather(scenario, waste_rate, -name, -stage)
+
+# jitter horizontally, showing baseline and optimal
+# waste_rate_df %>%
+#   filter(scenario %in% c('baseline', 'optimal')) %>%
+#   mutate(scen_jitter = jitter(as.numeric(factor(scenario)), factor = 0.1)) %>%
+# ggplot(aes(x = scen_jitter, y = waste_rate, color = stage, group = name)) +
+#   scale_x_continuous(breaks = 1:2, labels = factor(c('baseline', 'optimal'))) +
+#   scale_y_continuous(name = 'Waste rate', labels = scales::percent) +
+#   scale_color_discrete(labels = c('production', 'retail', 'consumption')) +
+#   geom_line(color = 'black') + geom_point() +
+#   ggtitle('Waste rates minimizing GHG emissions given total cost') +
+#   theme_bw() +
+#   theme(axis.title.x = element_blank(), 
+#         panel.grid.major.x = element_blank(), 
+#         panel.grid.minor.x = element_blank(),
+#         legend.position = 'bottom')
+  
+# Plot waste rates
+set.seed(111)
+waste_rate_df %>%
+  mutate(cost_jitter = jitter(total_cost, factor = 0.5)) %>%
+  ggplot(aes(x = cost_jitter, y = waste_rate, color = stage, group = name)) +
+  scale_x_continuous(name = 'Total invested in FLW reduction', breaks = c(0, 500, 1000, 2000, 5000)) +
+  scale_y_continuous(name = 'Waste rate', labels = scales::percent, expand = c(0,0), limits = c(0, 0.27)) +
+  scale_color_brewer(type='qual', palette='Set2', labels = c('production', 'retail', 'consumption')) +
+  geom_line(color = 'black') + geom_point() +
+  ggtitle('Waste rates minimizing GHG emissions given total cost') +
+  theme_bw() +
+  theme(axis.title.x = element_blank(), 
+        panel.grid.major.x = element_blank(), 
+        panel.grid.minor.x = element_blank(),
+        legend.position = 'bottom')
+ggsave('/nfs/qread-data/figures/costcurve_fake_example_ratesbycost.png', height = 6, width = 6, dpi = 300)
+
+# Plot optimal solutions
+optimal_df <- map2_dfr(optim_bytotals, Ctotal_vec, ~ data.frame(total_cost = .y, stage = factor(c('production', 'retail', 'consumption'), levels = c('production', 'retail', 'consumption')), cost = .x$pars))
+
+ggplot(optimal_df, aes(x = total_cost, y = cost, color = stage, group = stage)) +
+  geom_point(size = 2) + geom_line() +
+  scale_x_continuous(name = 'Total invested in FLW reduction', breaks = c(0, 500, 1000, 2000, 5000)) +
+  scale_y_continuous(name = 'Amount invested in each stage') +
+  scale_color_brewer(type='qual', palette='Set2') +
+  ggtitle('Optimal allocation of FLW reduction funds to minimize GHG emissions', 'for a number of possible total investments') +
+  theme_bw() +
+  theme(axis.title.x = element_blank(), 
+       panel.grid.major.x = element_blank(), 
+       panel.grid.minor.x = element_blank(),
+       legend.position = 'bottom')
+ggsave('/nfs/qread-data/figures/costcurve_fake_example_allocationsbycost.png', height = 6, width = 6, dpi = 300)
