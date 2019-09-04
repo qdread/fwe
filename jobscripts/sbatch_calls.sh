79c253c8e9d5542b3436a15d6fc9273fe18232c2
Quentin Read <quentin.read@gmail.com>
2019-09-04 12:01:09 -0400

    Reprojection script now extracts proj4string instead of hardcoding it

--- jobscripts/sbatch_calls.sh
+++ jobscripts/sbatch_calls.sh
@@ -3,4 +3,4 @@ cd ~/fwe/jobscripts
 # Extract NLCD2016 by combined BCR, FAF, and TNC
 sbatch --export=vector_file=/nfs/fwe-data/landuse/ecoregions/bcr_usa_combined.shp,raster_file=/nfs/fwe-data/landuse/NLCD/nlcd2016landcover.vrt,output_file=/nfs/fwe-data/landuse/NLCD/NLCD_2016_BCR.csv countpixels.sh
 sbatch --export=vector_file=/nfs/fwe-data/commodity_flows/FAF/Freight_Analysis_Framework_Regions/faf_aea.shp,raster_file=/nfs/fwe-data/landuse/NLCD/nlcd2016landcover.vrt,output_file=/nfs/fwe-data/landuse/NLCD/NLCD_2016_FAF.csv countpixels.sh
-sbatch --export=vector_file=/nfs/fwe-data/landuse/ecoregions/tnc_usa.shp,raster_file=/nfs/fwe-data/landuse/NLCD/nlcd2016landcover.vrt,output_file=/nfs/fwe-data/landuse/NLCD/NLCD_2016_TNC.csv countpixels.sh
+sbatch --export=vector_file=/nfs/fwe-data/landuse/ecoregions/tnc_usa_aea.shp,raster_file=/nfs/fwe-data/landuse/NLCD/nlcd2016landcover.vrt,output_file=/nfs/fwe-data/landuse/NLCD/NLCD_2016_TNC.csv countpixels.sh
--- read_data/transform_cfs.sh
+++ read_data/transform_cfs.sh
@@ -1,7 +1,12 @@
 # Extract projection from NLCD raster to prj file, then use it to transform the FAF shapefile to that projection
+# Also transform the TNC USA shapefile
 
-# gdalsrsinfo /nfs/fwe-data/landuse/NLCD/nlcd2016landcover.vrt > /nfs/fwe-data/landuse/NLCD/nlcd.prj
-# gdalwarp -t_srs "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs" /nfs/fwe-data/commodity_flows/FAF/Freight_Analysis_Framework_Regions/Freight_Analysis_Framework_Regions.shp /nfs/fwe-data/commodity_flows/FAF/Freight_Analysis_Framework_Regions/faf_aea.shp
+# Must remove trailing spaces in projections with xargs
 
-ogr2ogr -f "ESRI Shapefile" -t_srs "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs" -s_srs EPSG:4326 /nfs/fwe-data/commodity_flows/FAF/Freight_Analysis_Framework_Regions/faf_aea.shp /nfs/fwe-data/commodity_flows/FAF/Freight_Analysis_Framework_Regions/Freight_Analysis_Framework_Regions.shp
+nlcdproj=`gdalsrsinfo /nfs/fwe-data/landuse/NLCD/nlcd2016landcover.vrt -o proj4 | xargs`
 
+ogr2ogr -f "ESRI Shapefile" -t_srs "${nlcdproj}" -s_srs EPSG:4326 /nfs/fwe-data/commodity_flows/FAF/Freight_Analysis_Framework_Regions/faf_aea.shp /nfs/fwe-data/commodity_flows/FAF/Freight_Analysis_Framework_Regions/Freight_Analysis_Framework_Regions.shp
+
+tncproj=`gdalsrsinfo /nfs/fwe-data/landuse/ecoregions/tnc_usa.prj -o proj4 | xargs`
+
+ogr2ogr -f "ESRI Shapefile" -t_srs "${nlcdproj}" -s_srs "${tncproj}" /nfs/fwe-data/landuse/ecoregions/tnc_usa_aea.shp /nfs/fwe-data/landuse/ecoregions/tnc_usa.shp
