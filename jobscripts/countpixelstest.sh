a88a5a398cf1b02922702adb23208255352dd7d8
Quentin Read <qread@sshgw02.research.sesync.org>
2019-09-04 09:43:20 -0400

    update jobscripts

--- jobscripts/countpixelstest.sh
+++ jobscripts/countpixelstest.sh
@@ -5,5 +5,5 @@
 
 # CFS shapefile: tabulate NLCD land uses
 # Use the FAF one since for some reason it works much quicker despite having a lot more regions
-python3 /research-home/qread/fwe/NLCD/tabulateraster.py /nfs/fwe-data/commodity_flows/FAF/Freight_Analysis_Framework_Regions/faf_aea.shp /nfs/fwe-data/landuse/NLCD/nlcd2016landcover.vrt /nfs/fwe-data/landuse/NLCD/NLCD_2016_FAF.csv
+python3 /research-home/qread/fwe/NLCD/tabulateraster.py /nfs/fwe-data/commodity_flows/FAF/Freight_Analysis_Framework_Regions/faf_aea.shp /nfs/fwe-data/landuse/NLCD/nlcd2011landcover.vrt /nfs/fwe-data/landuse/NLCD/NLCD_2011_FAF.csv
 
