261b07954288809b34d6f6ce2a0a1bd3015eaefa
Quentin Read <quentin.read@gmail.com>
2019-09-04 09:43:52 -0400

    Shell scripts to transform NLCD and FAF spatial data

--- NLCD/makenlcdvrts.sh
+++ NLCD/makenlcdvrts.sh
@@ -9,3 +9,6 @@ gdalbuildvrt /nfs/qread-data/NLCD/nlcd2006landcover.vrt /nfs/public-data/NLCD/nl
 gdalbuildvrt /nfs/qread-data/NLCD/nlcd2011landcover.vrt /nfs/public-data/NLCD/nlcd_2011_landcover_2011_edition_2014_03_31/nlcd_2011_landcover_2011_edition_2014_03_31.img
 gdalbuildvrt /nfs/qread-data/NLCD/nlcd20012006change.vrt /nfs/public-data/NLCD/nlcd_2001_to_2006_landcover_fromto_change_index_2011_edition_2014_04_09/nlcd_2001_to_2006_landcover_fromto_change_index_2011_edition_2014_04_09.img
 gdalbuildvrt /nfs/qread-data/NLCD/nlcd20062011change.vrt /nfs/public-data/NLCD/nlcd_2006_to_2011_landcover_fromto_change_index_2011_edition_2014_04_09/nlcd_2006_to_2011_landcover_fromto_change_index_2011_edition_2014_04_09.img
+
+# Edit: 2016 VRT
+gdalbuildvrt /nfs/fwe-data/landuse/NLCD/nlcd2016landcover.vrt /nfs/fwe-data/landuse/NLCD/NLCD_2016_Land_Cover_L48_20190424.img
