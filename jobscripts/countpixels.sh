df52da46917e71e91e915a80334ea9c0e81bd3df
Quentin Read <quentin.read@gmail.com>
2019-09-04 11:48:01 -0400

    Add some new sbatch calls

--- jobscripts/countpixels.sh
+++ jobscripts/countpixels.sh
@@ -3,6 +3,5 @@
 #SBATCH --partition=sesyncshared
 #SBATCH --job-name=countpxls
 
-# CFS shapefile: tabulate NLCD land uses
-# Use the FAF one since for some reason it works much quicker despite having a lot more regions
+# Arguments with the file name of the two inputs and the output (to be created) passed with --export flag of sbatch
 python3 /research-home/qread/fwe/NLCD/tabulateraster.py ${vector_file} ${raster_file} ${output_file} 
