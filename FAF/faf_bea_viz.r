06c19d12f439bd0412d1d650cd7e2b4feae315e9
Quentin Read <qread@sshgw02.research.sesync.org>
2019-10-08 09:26:15 -0400

    Convert cfs polygon file to GPKG From now on I will use GeoPackage for all vector files, not shp GeoJSON doesn't seem to work well either

--- FAF/faf_bea_viz.r
+++ FAF/faf_bea_viz.r
@@ -15,10 +15,36 @@ fp_crosswalk <- file.path(ifelse(dir.exists('Q:/'), 'Q:', '/nfs/qread-data'), 'c
 fp_out <- file.path(ifelse(dir.exists('Q:/'), 'Q:', '/nfs/qread-data'), 'cfs_io_analysis')
 
 load(file.path(fp_out, 'faf_by_bea.RData'))
-cfsmap <- st_read(dsn = file.path(fp_faf, 'Freight_Analysis_Framework_Regions/cfs_aea.geojson'))
+cfsmap <- st_read(dsn = file.path(fp_faf, 'Freight_Analysis_Framework_Regions/cfs_aea.gpkg'))
 faf_lookup <- read_csv(file.path(fp_out, 'faf_region_lookup.csv'))
 
 
 # Summarize faf data for map ----------------------------------------------
 
+# Domestic outbound (wide)
+faf_outbound <- faf_by_bea %>%
+  filter(trade_type == 1) %>%
+  group_by(dms_orig, BEA_code) %>%
+  summarize(value = sum(value_2012)) %>%
+  spread(BEA_code, value)
 
+cfsmap_outbound <- cfsmap %>% left_join(faf_outbound, by = c('Code' = 'dms_orig'))
+
+faf_inbound <- faf_by_bea %>%
+  filter(trade_type == 1) %>%
+  group_by(dms_dest, BEA_code) %>%
+  summarize(value = sum(value_2012)) %>%
+  spread(BEA_code, value)
+
+cfsmap_inbound <- cfsmap %>% left_join(faf_inbound, by = c('Code' = 'dms_dest'))
+
+
+# Make some maps ----------------------------------------------------------
+
+# If we want to exclude AK, HI
+ak_hi_idx <- grepl('Alaska|Hawaii|Honolulu', cfsmap$FAF_Region)
+
+plot(cfsmap_outbound[c('1111A0', '1111B0', '111200', '111300')])
+
+ggplot(cfsmap_outbound %>% filter(!ak_hi_idx)) +
+  geom_sf(aes(fill = `1111A0`))
--- FAF/fix_map_codes.r
+++ FAF/fix_map_codes.r
@@ -1,4 +1,6 @@
 # Correct CFS map to add codes and corrected names
+# Also from now on all vector files should be saved as OGC GeoPackage, see http://switchfromshapefile.org/
+
 
 library(tidyverse)
 library(sf)
@@ -10,7 +12,6 @@ fp_satellite <- file.path(fp, 'IO_tables/output_csvs')
 fp_crosswalk <- file.path(ifelse(dir.exists('Q:/'), 'Q:', '/nfs/qread-data'), 'crossreference_tables')
 fp_out <- file.path(ifelse(dir.exists('Q:/'), 'Q:', '/nfs/qread-data'), 'cfs_io_analysis')
 
-load(file.path(fp_out, 'faf_by_bea.RData'))
 cfsmap <- st_read(dsn = file.path(fp_faf, 'Freight_Analysis_Framework_Regions'), layer = 'cfs_aea') %>% 
   mutate(FAF_Region = gsub('[ ]{2,}', ' ', as.character(CFS12_NAME)))
 faf_lookup <- read_csv(file.path(fp_faf, 'faf4_region_lookup.csv')) %>%
@@ -26,6 +27,6 @@ cfsmap$FAF_Region[cfsmap$FAF_Region %in% c('Remainder of Alaska', 'Remainder of 
 
 # Write corrected FAF lookup table and cfs map joined (write as geojson)
 cfsmap <- cfsmap %>% left_join(faf_lookup)
-st_write(cfsmap, dsn = file.path(fp_faf, 'Freight_Analysis_Framework_Regions','cfs_aea.geojson'), driver = 'GeoJSON')
+st_write(cfsmap, dsn = file.path(fp_faf, 'Freight_Analysis_Framework_Regions','cfs_aea.gpkg'), driver = 'GPKG')
 
 write_csv(faf_lookup, file.path(fp_out, 'faf_region_lookup.csv'))
