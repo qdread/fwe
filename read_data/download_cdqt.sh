0a04551e96631b73a8144921a80aa9d4e473a2cf
Quentin Read <quentin.read@gmail.com>
2019-12-04 15:28:25 -0500

    Download 2017 cdqt data also

--- read_data/download_cdqt.sh
+++ read_data/download_cdqt.sh
@@ -1,7 +1,10 @@
 # Download some USDA data 
 # 24 Oct 2019
 
-# Census Data Query Tool
+# Census Data Query Tool 2012 and 2017
 cd /nfs/fwe-data/USDA
 wget https://www.nass.usda.gov/Publications/AgCensus/2012/Online_Resources/Census_Data_Query_Tool/2012_cdqt_data.txt.gz
 gunzip 2012_cdqt_data.txt.gz
+
+wget https://www.nass.usda.gov/Publications/AgCensus/2017/Online_Resources/Census_Data_Query_Tool/2017_cdqt_data.txt.gz
+gunzip 2017_cdqt_data.txt.gz
