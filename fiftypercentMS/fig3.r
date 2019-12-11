efcb3ec68a85c2d183c82533115c843a6fd656a2
Quentin Read <quentin.read@gmail.com>
2019-12-11 16:40:25 -0500

    Start individual scripts for figs 3 and 4 in MS

---
+++ fiftypercentMS/fig3.r
@@ -0,0 +1,110 @@
+# Figure 3 in MS
+# Code taken from sixstage_sens_figs.r
+# QDR / FWE / 11 Dec 2019
+
+library(tidyverse)
+fp <- ifelse(dir.exists('Q:/'), 'Q:', '/nfs/qread-data')
+fpfig <- file.path(fp, 'figures') 
+fp_output <- file.path(fp, 'scenario_results')
+
+stage_full_names <- c('production', 'processing', 'retail', 'consumption: foodservice', 'consumption: institutional', 'consumption: household')
+
+optimal_df_all <- read.csv(file.path(fp_output, 'sixstage_scenario_opt_results.csv'))
+grid_result <- read.csv(file.path(fp_output, 'sixstage_scenario_grid_lcia_results.csv'), stringsAsFactors = FALSE)
+grid_sensitivity_df <- read.csv(file.path(fp_output, 'sensitivity_grid_alldraws.csv'), stringsAsFactors = FALSE)
+grid_sensitivity_CIs <- read.csv(file.path(fp_output, 'sensitivity_grid_CIs.csv'), stringsAsFactors = FALSE)
+
+stage_full_names_lookup <- c(none = '', L1 = 'production', L2 = 'processing', L3 = 'retail', L4a = 'consumption:\nfoodservice', L4b = 'consumption:\ninstitutional', L5 = 'consumption:\nhousehold')
+
+# Calculate sequences for each draw ---------------------------------------
+
+create_sequence <- function(dat, value_id, proportion) {
+  value_id <- enquo(value_id)
+  dat %>%
+    ungroup %>%
+    mutate(nbypct = rowSums(.[,1:6] == proportion)) %>%
+    filter(rowSums(.[,1:6] > 0) == nbypct) %>%
+    group_by(nbypct) %>%
+    filter(!!value_id == min(!!value_id)) %>%
+    arrange(nbypct) %>%
+    ungroup %>%
+    mutate(stage_reduced = c('none', names(sort(apply(.[,1:6],2,function(x) which(diff(x) > 0))))))
+}
+
+# Do all sequences for each parameter draw.
+
+# All sequences, all categories
+allseq_allcats <- grid_sensitivity_df %>%
+  filter(grepl('co2|watr|land|enrg|eutr', impact_category)) %>%
+  group_by(draw_id, impact_category) %>%
+  do(create_sequence(., value, 0.5))
+
+# Do the sequence for the "real" values of the parameters.
+trueseq <- grid_result %>%
+  filter(grepl('co2|watr|land|enrg|eutr', impact_category)) %>%
+  group_by(impact_category) %>%
+  do(create_sequence(., value, 0.5))
+
+# Match the true sequence values with the error bar values.
+trueseq_withcis <- trueseq %>% left_join(grid_sensitivity_CIs)
+
+# Function to create reduction plot with error bars
+reduction_plot_with_errorbars <- function(sequence, yval, yminval, ymaxval, plot_title, plot_subtitle) {
+  yval <- enquo(yval)
+  yminval <- enquo(yminval)
+  ymaxval <- enquo(ymaxval)
+  sequence %>%
+    mutate(norm = max(!!yval)) %>%
+    mutate(!!yval := !!yval/norm, 
+           !!yminval := !!yminval/norm,
+           !!ymaxval := !!ymaxval/norm) %>%
+    mutate(stage_reduced = stage_full_names_lookup[stage_reduced]) %>%
+    ggplot(aes(x = nbypct, y = !!yval, ymin = !!yminval, ymax = !!ymaxval)) +
+    geom_line(size = 1) +
+    geom_errorbar(width = 0.2, color = 'gray60') +
+    geom_point(size = 3, color = 'white', fill = 'black', shape = 21, stroke = 2) +
+    geom_text(aes(label = stage_reduced), angle = 45, hjust = 1.25) +
+    scale_x_continuous(name = 'Number of stages where waste is reduced', breaks = 0:6) +
+    scale_y_continuous(name = 'Impact relative to baseline', labels = scales::percent_format(accuracy=1)) +
+    coord_cartesian(ylim = c(0.77, 1.02), expand = TRUE) +
+    ggtitle(plot_title, plot_subtitle) +
+    theme_bw() +
+    theme(panel.grid.minor.x = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          panel.grid.major.x = element_blank(),
+          plot.subtitle = element_text(size = 10))
+  
+}
+
+subtitle <- 'done by sequentially reducing waste in the FSC stage \nwith greatest impact by 50%'
+
+# Do this as a facet wrap plot
+
+facetplotdat <- trueseq_withcis %>%
+  filter(grepl('co2|eutr|land|watr|enrg', impact_category)) %>%
+  group_by(impact_category) %>%
+  mutate(value_norm = value/max(value),
+         q025_norm = q025/max(value),
+         q975_norm = q975/max(value)) %>%
+  ungroup %>%
+  mutate(stage_reduced = stage_full_names_lookup[stage_reduced],
+         impact_category = factor(impact_category, labels = c('eutrophication potential', 'greenhouse warming potential', 'energy use', 'land use', 'water use'))) 
+
+facetplot <- ggplot(facetplotdat %>% mutate(letter = letters[1:5][impact_category]), aes(x = nbypct, y = value_norm, ymin = q025_norm, ymax = q975_norm)) +
+  geom_line(size = 1) +
+  geom_errorbar(width = 0.2, color = 'gray60') +
+  geom_point(size = 3, color = 'white', fill = 'black', shape = 21, stroke = 2) +
+  geom_text(aes(label = stage_reduced), angle = 45, hjust = 1.25, size = 3) +
+  geom_text(aes(label = letter), x = 0, y = 0.78, size = 12) +
+  scale_x_continuous(name = 'Number of stages where waste is reduced', breaks = 0:6) +
+  scale_y_continuous(name = 'Impact relative to baseline', labels = scales::percent_format(accuracy = 1L)) +
+  facet_wrap(~ impact_category) +
+  coord_cartesian(ylim = c(0.77, 1.02), expand = TRUE) +
+  theme_bw() +
+  theme(panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        plot.subtitle = element_text(size = 10),
+        strip.background = element_blank())
+
+ggsave(file.path(fpfig, 'sixstage_facetplot.png'), facetplot, height = 8.5, width = 11.5, dpi = 300)
---
+++ fiftypercentMS/fig4.r
@@ -0,0 +1,115 @@
+# New figure 4 in MS: multipanel Figure 4.
+# Using code taken from pathway_commodity_plots.r
+# QDR / FWE / 11 Dec 2019
+
+# Load data ---------------------------------------------------------------
+
+fp_output <- file.path(ifelse(dir.exists('Q:/'), 'Q:', '/nfs/qread-data'), 'scenario_results')
+fp_github <- file.path(ifelse(dir.exists('~/fwe'), '~/fwe', '~/Documents/GitHub/fwe'))
+fp_fig <- file.path(ifelse(dir.exists('Q:/'), 'Q:', '/nfs/qread-data'), 'figures')
+
+library(tidyverse)
+library(ggrepel)
+
+results_by_commodity_df <- read.csv(file.path(fp_output, 'bestpathway_bycommodity.csv'), stringsAsFactors = FALSE)
+
+# Create figures ----------------------------------------------------------
+
+
+# Map stage, category, and food commodity group labels to more descriptive labels
+stage_full_names_lookup <- c(none = '', L1 = 'production', L2 = 'processing', L3 = 'retail', L4a = 'consumption:\nfood service', L4b = 'consumption:\ninstitutional', L5 = 'consumption:\nhousehold')
+categories <- c("impact potential/gcc/kg co2 eq", "resource use/land/m2*yr", "resource use/watr/m3", "resource use/enrg/mj", "impact potential/eutr/kg n eq")
+category_short <- c('GHG', 'land', 'water', 'energy', 'eutrophication')
+food_categories <- c("cereals", "roots_tubers_fresh", "roots_tubers_processed", 
+                     "oilseeds_pulses", "fruit_veg_fresh", "fruit_veg_processed", 
+                     "meat", "fish_fresh", "fish_processed", "milk", "eggs", "sugar", 
+                     "beverages")
+food_categories_labels <- c('cereals', 'roots and tubers (fresh)', 'roots and tubers (processed)', 'oilseeds and pulses', 'fruits and vegetables (fresh)', 'fruits and vegetables (processed)', 'meat', 'fish (fresh)', 'fish (processed)', 'milk', 'eggs', 'sugar', 'beverages')
+
+results_by_commodity_df <- results_by_commodity_df %>%
+  mutate(category = category_short[match(category, categories)],
+         stage_reduced = stage_full_names_lookup[stage_reduced],
+         commodity = food_categories_labels[match(commodity, food_categories)]) 
+
+# Add baseline case for plotting
+baseline_df <- data.frame(category = 'all', commodity = unique(results_by_commodity_df$commodity), n_stages_reduced = 0, stage_reduced = 'none', impact = 1, baseline_impact = 1, impact_norm = 1, stringsAsFactors = FALSE)
+
+results_by_commodity_df <- rbind(results_by_commodity_df, baseline_df)
+
+
+# Figures showing only the top 5 commodities ------------------------------
+
+# Modify so we can show other numbers besides 5
+get_top_n <- function(data, the_category, n_comm = 5) {
+  data %>% 
+    filter(category %in% the_category) %>%
+    group_by(commodity) %>%
+    filter(n_stages_reduced == max(n_stages_reduced)) %>%
+    ungroup %>%
+    arrange(impact_norm) %>%
+    slice(1:n_comm) %>%
+    pull(commodity)
+}
+
+# Figures showing top 5 as impact averted ---------------------------------
+
+results_by_commodity_df <- results_by_commodity_df %>% 
+  mutate(impact_averted = baseline_impact - impact)
+
+# Convert axis values to a more legible unit by dividing by the 2012 USA population to get per capita impact
+pop2012 <- 314e6
+percapita_results <- results_by_commodity_df %>%
+  mutate_at(vars(impact, baseline_impact, impact_averted), ~ if_else(category%in% 'all', ., .x/pop2012))
+
+# Get all top 5 names to assign colors to them
+alltop5 <- map(category_short, ~ get_top_n(results_by_commodity_df, .x, 5)) %>% reduce(c) %>% unique
+alltop5_colormap <- setNames(RColorBrewer::brewer.pal(length(alltop5), 'Dark2'), alltop5)
+
+plot_impactaverted <- function(dat, the_category, unit_name, n = 5) {
+  dat <- dat %>% 
+    filter(commodity %in% get_top_n(., the_category, n), category %in% c('all', the_category))
+  
+  ggplot(dat, aes(x = n_stages_reduced, y = impact_averted, group = commodity)) + 
+    geom_line(size = 1, alpha = 0.33) +
+    geom_point(aes(color = commodity), size = 3) +
+    geom_text_repel(data = dat %>% filter(n_stages_reduced > 0), aes(label = stage_reduced), size = 2) + # Label to see which stages are being averted
+    scale_x_continuous(name = 'Number of stages where waste is reduced', breaks = 0:6) +
+    scale_y_continuous(name = parse(text = paste('Per~capita~impact~averted:', unit_name, sep = '~')), 
+                       limits = c(0, max(dat$impact_averted) * 1.05), expand = c(0, 0),
+                       sec.axis = sec_axis(trans = ~ ./dat$baseline_impact[1], name = 'relative to baseline', labels = scales::percent)) +
+    scale_color_manual(name = 'Food group', values = alltop5_colormap, guide = guide_legend(nrow = 2)) +
+    theme_bw() +
+    theme(panel.grid.minor.x = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          panel.grid.major.x = element_blank(),
+          legend.position = 'bottom')
+}
+
+p_ghg <- plot_impactaverted(percapita_results, 'GHG', 'GHG~(kg~CO^2~eq.~y^-1)') + annotate('text',x=-Inf,y=Inf,hjust=-0.2,vjust=1.2,label='GHG')
+p_land <- plot_impactaverted(percapita_results, 'land', 'land~(m^2~y^-1)') + annotate('text',x=-Inf,y=Inf,hjust=-0.2,vjust=1.2,label='land')
+p_water <- plot_impactaverted(percapita_results, 'water', 'water~(m^3~y^-1)') + annotate('text',x=-Inf,y=Inf,hjust=-0.2,vjust=1.2,label='water')
+p_energy <- plot_impactaverted(percapita_results, 'energy', 'energy~(MJ~y^-1)') + annotate('text',x=-Inf,y=Inf,hjust=-0.2,vjust=1.2,label='energy')
+p_eutr <- plot_impactaverted(percapita_results, 'eutrophication', 'eutrophication~potential~(kg~N~eq.~y^-1)') + annotate('text',x=-Inf,y=Inf,hjust=-0.2,vjust=1.2,label='eutrophication')
+
+# Make a legend for all the plots.
+library(cowplot)
+
+leg_plot <- ggplot(percapita_results %>% filter(commodity %in% alltop5), aes(x=1, y=1, color=commodity)) +
+  geom_point() +
+  theme_bw() +
+  scale_color_manual(name = 'Food group', values = alltop5_colormap, guide = guide_legend(nrow = 4))
+
+leg <- get_legend(leg_plot)
+
+# Lay out the 5 plots plus legend as a 3 x 2 grid.
+
+th_top <- theme(legend.position = 'none',
+                axis.text.x = element_blank(),
+                axis.title.x = element_blank(),
+                axis.ticks.x = element_blank())
+th_bottom <- theme(legend.position = 'none')
+
+top_row <- plot_grid(p_eutr + th_top, p_ghg + th_top, p_energy + th_top, nrow = 1)
+bottom_row <- plot_grid(p_land + th_bottom, p_water + th_bottom, leg, nrow = 1)
+
+whole_plot <- plot_grid(top_row, bottom_row, nrow = 2)
