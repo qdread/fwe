1165f222afae1e431c821f4dcff5b46fb929b475
qdread <qread@sesync.org>
2020-09-21 09:51:28 -0400

    update read_lafa to be teh tidyverse

--- read_data/read_lafa.r
+++ read_data/read_lafa.r
@@ -1,26 +1,28 @@
 # Script for reading LAFA XLSX files into R
 # QDR/FWE/01 Oct 2018
 
+# Updated 21 Sep 2020 to use readxl
 # Updated 02 Nov 2018 to run both remotely and locally
 
 # Define functions --------------------------------------------------------
 
 # Function to read single sheet
-read_lafa_sheet <- function(name, wb) {
-  sheet_raw <- readWorksheet(wb, sheet = name)
+read_lafa_sheet <- function(name, file) {
+  sheet_raw <- suppressMessages(read_xls(file, sheet = name))
   # Find the index of the first and last row that contains a year number in the first column.
-  numeric_col1 <- as.numeric(str_extract_all(sheet_raw[,1], '^[0-9]{4}', simplify = TRUE))
+  numeric_col1 <- as.numeric(str_extract_all(pull(sheet_raw, 1), '^[0-9]{4}', simplify = TRUE))
   year_idx <- c(which.min(numeric_col1), which.max(numeric_col1))
   # Read the sheet again with only those rows.
-  sheet_dat <- readWorksheet(wb, 
-                             sheet = name, 
-                             startRow = year_idx[1] + 1, 
-                             endRow = year_idx[2] + 1, 
-                             header = FALSE,
-                             colTypes = 'numeric',
-                             forceConversion = TRUE)
+  sheet_dat <- suppressMessages(
+    read_xls(file, 
+             sheet = name, 
+             skip = year_idx[1],
+             n_max = diff(year_idx) + 1,
+             col_names = FALSE,
+             col_types = 'numeric'))
+  
   # Get the row with units in it.
-  unit_row <- readWorksheet(wb, sheet = name, startRow = year_idx[1], endRow = year_idx[1], header = FALSE)
+  unit_row <- suppressMessages(read_xls(file, sheet = name, skip = year_idx[1] - 1, n_max = 1, col_names = FALSE))
   # Parse it
   unit_row_parsed <- trimws(gsub('-', '', as.character(unit_row)))
   unit_row_parsed <- gsub('/', '.', unit_row_parsed)
@@ -28,7 +30,7 @@ read_lafa_sheet <- function(name, wb) {
   unit_row_parsed <- gsub('[^a-zA-Z0-9]*$', '', unit_row_parsed)
   
   # Read in header rows (row1 always has a title)
-  header_rows <- readWorksheet(wb, sheet = name, startRow = 2, endRow = 3, header = FALSE)
+  header_rows <- suppressMessages(read_xls(file, sheet = name, skip = 1, n_max = 2, col_names = FALSE))
   # Fill first header row forward if there is a NA
   header_row1 <- na.locf(unlist(header_rows[1,]))
   # Paste second header row onto first, if it exists
@@ -46,28 +48,23 @@ read_lafa_sheet <- function(name, wb) {
 
 # Function to read entire workbook
 read_lafa_workbook <- function(file) {
-  require(XLConnect)
-  require(stringr)
+  require(tidyverse)
+  require(readxl)
   require(zoo)
-  require(purrr)
-  require(dplyr)
   
-  wb <- loadWorkbook(file, create = TRUE)
-  
-  # Get sheet names and remove table of contents
-  sheet_names <- getSheets(wb)
+    # Get sheet names and remove table of contents
+  sheet_names <- excel_sheets(file)
   sheet_names <- sheet_names[!sheet_names %in% c('TableOfContents')]
   
-  all_sheets <- map(sheet_names, read_lafa_sheet, wb = wb)
-  all_sheets <- map2(sheet_names, all_sheets, function(x, y) data.frame(Category = x, y, stringsAsFactors = FALSE))
+  all_sheets <- map_dfr(sheet_names, ~ data.frame(Category = ., read_lafa_sheet(., file = file), stringsAsFactors = FALSE))
   
-  bind_rows(all_sheets)
+  return(all_sheets)
 }
 
 
 # Read data ---------------------------------------------------------------
 
-fp_lafa <- ifelse(dir.exists('Q:/'), 'Q:/raw_data/USDA/LAFA', ifelse(dir.exists('/nfs/qread-data'), '/nfs/qread-data/raw_data/USDA/LAFA', '~/Dropbox/projects/foodwaste/Data/LAFA_localcopy'))
+fp_lafa <- ifelse(dir.exists('Q:/'), 'Q:/raw_data/USDA/LAFA', ifelse(dir.exists('/nfs/qread-data'), '/nfs/qread-data/raw_data/USDA/LAFA', '~/Dropbox/Q/projects/foodwaste/Data/LAFA_localcopy'))
 
 dairy <- read_lafa_workbook(file.path(fp_lafa, 'Dairy.xls'))
 fat <- read_lafa_workbook(file.path(fp_lafa, 'fat.xls'))
